9680f7fd71f5856a31c54d1d85af28dcc957522f
KLO1HC <long.k@vn.bosch.com>
2024-07-09 19:00:55 +0700

    [Feature] Added BaseDbContext to Data.DataContext

---
+++ ProjectManagement.Infrastructure/Data/DataContext/WriteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManagement.Infrastructure.Data.DataContext
+{
+    public class WriteDbContext(DbContextOptions<WriteDbContext> dbOptions) : BaseDbContext<WriteDbContext>(dbOptions)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
